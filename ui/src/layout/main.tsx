7bd06438542bddf4a2ba0c2c0c065abd3dbd32e2
macarrie <mathieu+github@macarrie.fr>
2022-08-21 09:23:38 +0200

    DOCKER: Add ssh keys in server container build

--- ui/src/layout/main.tsx
+++ ui/src/layout/main.tsx
@@ -5,11 +5,13 @@ import TopBar from "./top_bar"
 class Main extends React.Component<any, any> {
     render() {
         return (
-            <div className="relative ml-12 md:ml-48 flex flex-col container bg-slate-50 py-4 px-4">
+            <div className="relative w-full ml-12 md:ml-48 flex flex-col bg-slate-50 py-4 px-4">
                 <TopBar />
                 <hr className="mb-4" />
 
-                {this.props.children}
+                <div className="container">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
